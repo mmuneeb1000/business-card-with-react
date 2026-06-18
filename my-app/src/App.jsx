0dd5a213b70c2ae0e633a658133c9e03d5ba40ce
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-19 01:18:33 +0500

    filename debug

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Info from "./components/Info";
-import Interest from "./components/Interest";
+import About from "./src/components/About";
+import Footer from "./src/components/Footer";
+import Info from "./src/components/Info";
+import Interest from "./src/components/Interest";
 
 function App() {
   return (
