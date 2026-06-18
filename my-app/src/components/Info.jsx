75a6504287495d31ee2add3b7d57de92bd442f79
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-19 01:38:57 +0500

    image fix

--- my-app/src/components/Info.jsx
+++ my-app/src/components/Info.jsx
@@ -1,9 +1,9 @@
-import profile from "./src/images/profile-id.jpg";
+import profile from "../images/profile-id.jpg";
 
 export default function Info() {
   return (
     <section className="head-section">
-      <img src="{profile}" />
+      <img src={profile} alt="colorful background, developer at work" />
       <h1>M. Muneeb</h1>
       <h3>Frontend Developer</h3>
       <a className="website-link" href="https://www.webdevpk.store">
