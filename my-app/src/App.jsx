087d36bd96aecde0b0336f9cd466cbfe3474ecc5
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-19 01:21:56 +0500

    deploy fix APP

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
-import About from "./src/components/About";
-import Footer from "./src/components/Footer";
-import Info from "./src/components/Info";
-import Interest from "./src/components/Interest";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
+import Interest from "./components/Interest";
 
 function App() {
   return (
